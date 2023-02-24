cff713175d6053faff591d55c31dc9eefb750adf
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-24 16:20:16 +0200

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -2,6 +2,18 @@ function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
 
+const page = document.body;
+const color = document.querySelector(".color");
+const button = document.querySelector(".change-color");
+
+const changeColor = event => {
+  let resultColor = getRandomHexColor();
+  page.style.backgroundColor = resultColor;
+  color.textContent = resultColor;
+}
+
+button.addEventListener("click", changeColor);
+
 /*
 Write a script that, after clicking the "Start" button, 
 changes the <body> background color once a second to a random value using the inline style. 
