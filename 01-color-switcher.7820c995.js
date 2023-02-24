8f7c36902e03a04fb8053a2badc25cb6abc2bc9f
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-24 15:06:49 +0000

    Deploying to gh-pages from @ sandra-selezen/goit-js-hw-09@72e5bad3d60cff9a1cdb88d680d236ece155cfae 🚀

--- 01-color-switcher.7820c995.js
+++
@@ -1,2 +0,0 @@
-const o=document.body,t=document.querySelector(".color");document.querySelector(".change-color").addEventListener("click",(e=>{let c=`#${Math.floor(16777215*Math.random()).toString(16)}`;o.style.backgroundColor=c,t.textContent=c}));
-//# sourceMappingURL=01-color-switcher.7820c995.js.map
--- 01-color-switcher.d6b5f453.js
+++
@@ -1,2 +0,0 @@
-!function(){var o=document.body,t=document.querySelector(".color");document.querySelector(".change-color").addEventListener("click",(function(e){var n="#".concat(Math.floor(16777215*Math.random()).toString(16));o.style.backgroundColor=n,t.textContent=n}))}();
-//# sourceMappingURL=01-color-switcher.d6b5f453.js.map
