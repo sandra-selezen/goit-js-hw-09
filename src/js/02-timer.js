f4e28e6a4c87b902756e9c7ae2901e9d4126319b
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-24 18:52:42 +0200

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -2,7 +2,14 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
-const dateInput = document.querySelector("#datetime-picker");
+const refs = {
+  dateInput: document.querySelector("#datetime-picker"),
+  btnStart: document.querySelector("[data-start]"),
+  days: document.querySelector("[data-days]"),
+  hours: document.querySelector("[data-hours]"),
+  minutes: document.querySelector("[data-minutes]"),
+  seconds: document.querySelector("[data-seconds]")
+};
 
 const options = {
   enableTime: true,
@@ -10,11 +17,15 @@ const options = {
   defaultDate: new Date(),
   minuteIncrement: 1,
   onClose(selectedDates) {
-  console.log(selectedDates[0]);
+    console.log(selectedDates[0]);
+    if (selectedDates[0] < Date.now()) {
+      refs.btnStart.setAttribute("disabled", true);
+      Notify.failure("Please choose a date in the future");
+    }
   },
 };
 
-const selectedDate = flatpickr(dateInput, options);
+flatpickr(refs.dateInput, options);
 
 function convertMs(ms) {
   // Number of milliseconds per unit of time
@@ -33,25 +44,22 @@ function convertMs(ms) {
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
-}
+};
 
-// function addLeadingZero(time) {
-  
-// }
+function addLeadingZero(value) {
+  return value.toString().padStart(2, '0');
+};
 
-// pad(value) {
-//   return String(value).padStart(2, '0');
-// }
-
-// function updateClockface({ days, hours, mins, secs }) {
-//   refs.clockface.textContent = `${days}:${hours}:${mins}:${secs}`;
-// }
+function updateClockface({ days, hours, minutes, seconds }) {
+  refs.days.textContent = addLeadingZero(days);
+  refs.hours.textContent = addLeadingZero(hours);
+  refs.minutes.textContent = addLeadingZero(minutes);
+  refs.seconds.textContent = addLeadingZero(seconds);
+};
 
 console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
 console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
 console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
-
-Notify.failure("Please choose a date in the future");
 
 /*
 Write a timer script that counts down to a specific date. 
