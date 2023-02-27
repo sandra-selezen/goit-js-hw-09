81f8dd9083095f5aef3fde715c33e234e7689052
sandra-selezen <113197805+sandra-selezen@users.noreply.github.com>
2023-02-27 19:45:25 +0200

    Add task #3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,12 +1,46 @@
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 
+const form = document.querySelector(".form");
+
+form.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+  // console.log(event.currentTarget.elements);
+  const { 
+    elements: { delay, step, amount }
+  } = event.currentTarget;
+
+  const dataForm = {
+    delay: Number(delay.value),
+    step: Number(step.value),
+    amount: Number(amount.value)
+  };
+
+  for (let i = 1; i <= dataForm.amount; i += 1) {
+    createPromise(i, dataForm.delay)
+    .then(({ position, delay }) => {
+      Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+    })
+    .catch(({ position, delay }) => {
+      Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+    });
+
+    dataForm.delay += dataForm.step;
+  }
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+  setTimeout(() => {
+    if (shouldResolve) {
+      resolve({ position, delay });
+    } else {
+      reject({ position, delay });
+    }
+  }, delay);
+});
 }
 
 /*
